7b477a7ccf7aba46e5a6175b5da76c4522fc03d6
Architekt <richardpennymon@gmail.com>
2022-10-31 17:30:57 -0400

    Game over

--- game.py
+++ game.py
@@ -6,8 +6,10 @@ print("Welcome to our game!")
 name = input("What's your name? ")
 number = random.randrange(1,100)
 guess = int(input("What's your guess? "))
+count = 1
 
 while guess != number:
+    count += 1
     if guess < number:
         print("too low")
         guess = int(input("What's your guess? "))
@@ -16,3 +18,4 @@ while guess != number:
         guess = int(input("What's your guess? "))
 
 print("you got it")
+print(f"{name}, it took you {count} tries.")
